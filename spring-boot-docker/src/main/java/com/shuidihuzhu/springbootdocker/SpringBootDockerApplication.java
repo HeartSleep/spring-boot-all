398cdc17ef74608f3115d5d1b73406b4be88d005
gehuadong <gehuadong@shuidihubao.com>
2020-01-21 17:24:54 +0800

    first init

---
+++ spring-boot-docker/src/main/java/com/shuidihuzhu/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,17 @@
+package com.shuidihuzhu.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gehuadong@shuidihuzhu.com
+ * @date 2020-01-21
+ */
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+}
